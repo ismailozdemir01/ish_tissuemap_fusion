2b0fe751cd7a223541d1d5d38a8b6ed36fd2951e
ismailozdemir01 <14152632+ismailozdemir01@users.noreply.github.com>
2026-09-06 00:27:17 +0300

    Add HealthTip model class

---
+++ lib/models/health_tip.dart
@@ -0,0 +1,15 @@
+class HealthTip {
+  final String id;
+  final String category;
+  final String title;
+  final String description;
+  final int priority;
+
+  HealthTip({
+    required this.id,
+    required this.category,
+    required this.title,
+    required this.description,
+    required this.priority,
+  });
+}
